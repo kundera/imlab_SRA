7c06283bc429b7a1591823e2aa81a18b3ade0533
SukhyunCho <chosh90@snu.ac.kr>
2016-05-11 14:23:52 +0900

    make partial_search (to IO point and to next point)

--- learner/actiongenerator.py
+++ learner/actiongenerator.py
@@ -1,6 +1,5 @@
 from problemIO import problemreader
-import action
-import reward
+import action, reward, solution
 import math
 
 
@@ -21,6 +20,20 @@ class ActionGenerator(object):
             loca.append(int((index - (size_h * size_v)) % size_v))
 
         return loca
+
+    def change_to_two_dimension1(self, rack_status, columnNum, floorNum):
+        result = [[0.0 for col in range(columnNum)] for row in range(floorNum)]
+        for row in range(floorNum):
+            for col in range(columnNum):
+                result[row][col] = rack_status[row * floorNum + col]
+        return result
+
+    def change_to_two_dimension2(self, rack_status, columnNum, floorNum):
+        result = [[0.0 for col in range(columnNum)] for row in range(floorNum)]
+        for row in range(floorNum):
+            for col in range(columnNum):
+                result[row][col] = rack_status[columnNum * floorNum + row * floorNum + col]
+        return result
 
     def right_greedy_search(self, rack, column, floor, sol):
         # leftward direction search
@@ -128,14 +141,50 @@ class ActionGenerator(object):
 
     def io_partial_search(self, rack, column, floor, sol):
         # close to I/O point
+        target = sol.loc[0]
+        lot = []
 
+        for a, item1 in enumerate(rack):
+            if item1 == -1:
+                loca1 = self.loca_calculate(a, column, floor)
+                if loca1[1] < target[1] and loca1[2] < target[2]:
+                    lot.append(loca1)
 
-        return
+        if lot == []:
+            return sol
+        else:
+            distance = 0
+            for b in range(len(lot)):
+                new_distance = math.sqrt(lot[b][1]-0) + math.sqrt(lot[b][2]-0)
+                if distance < new_distance:
+                    sol.loc[0] = lot[b]
+                    distance = new_distance
 
-    def partial_search(self, rack, column, floor, sol):
+            return sol
+
+    def next_partial_search(self, rack, column, floor, sol):
         # close to after point
+        target = sol.loc[0]
+        next_node = sol.loc[1]
+        lot = []
 
-        return
+        for a, item1 in enumerate(rack):
+            if item1 == -1:
+                loca1 = self.loca_calculate(a, column, floor)
+                if target[1] < loca1[1] < next_node[1] and target[2] < loca1[2] < next_node[2]:
+                    lot.append(loca1)
+
+        if lot == []:
+            return sol
+        else:
+            distance = 10000
+            for b in range(len(lot)):
+                new_distance = math.sqrt(lot[b][1]-target[1]) + math.sqrt(lot[b][2]-target[2])
+                if distance > new_distance:
+                    sol.loc[0] = lot[b]
+                    distance = new_distance
+
+            return sol
 
     def full_search(self, rack, column, floor, sol):
         # close to median value of the item
@@ -143,23 +192,19 @@ class ActionGenerator(object):
 
 if __name__ == '__main__':
 
-    test = problemreader.ProblemReader(15)
+    test = problemreader.ProblemReader(20)
     rs = test.get_problem(1).rack.status
     column = test.get_problem(1).rack.column
     floor = test.get_problem(1).rack.floor
 
+    make = ActionGenerator()
+    for a in range(floor):
+        print make.change_to_two_dimension1(rs, column, floor)[a], '    ', make.change_to_two_dimension2(rs, column, floor)[a]
+
     ts = action.action()
-    sol = ts.dijk(rs, column, floor, [700, 392], [3, 0])[0]
+    sol = ts.dijk(rs, column, floor, [700, 392], [24, 24])[0]
     print sol.type, sol.oper, sol.loc
 
-    make = ActionGenerator()
-
-    print 1
-    print make.left_greedy_search(rs, column, floor, sol).loc
-    print 2
-    print make.right_greedy_search(rs, column, floor, sol).loc
-    print 3
-    print make.top_greedy_search(rs, column, floor, sol).loc
-    print 4
-    print make.bottom_greedy_search(rs, column, floor, sol).loc
-    print 5
+    print make.next_partial_search(rs, column, floor, sol).loc
+    # for i in range(0, 5):
+    #    print make.generating_idx(rs, column, floor, sol, i)[0].loc, make.generating_idx(rs, column, floor, sol, i)[1]
