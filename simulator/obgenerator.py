0b282cc91eb9a6fff2713ce75a3f884f452077f6
chico2121 <chico2121@naver.com>
2016-05-16 14:47:59 +0900

    update obgenerator.py

--- simulator/obgenerator.py
+++ simulator/obgenerator.py
@@ -11,7 +11,7 @@ class ObservationGenerator(object):
 
     # DB Configuration
     DBAdress = 'imlab-ws2.snu.ac.kr'
-    DBName = 'ASRS_exercise'
+    DBName = 'ASRS_RL_DATA'
     DBID = 'ioasrs'
     DBPassward = '1wjdqhruddud'
 
@@ -23,7 +23,7 @@ class ObservationGenerator(object):
         sql = "CREATE TABLE IF NOT EXISTS `%s`" \
               "(" \
               "idx int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT," \
-              "pidx int(5) NULL," \
+              "iteridx int(10) NULL," \
               "rs TEXT NULL COLLATE utf8mb4_unicode_ci," \
               "act int(2) NULL DEFAULT NULL," \
               "inp TEXT NULL COLLATE utf8mb4_unicode_ci," \
@@ -51,7 +51,8 @@ class ObservationGenerator(object):
 
         simul = nextstate.simul()
 
-        for idx in range(4):
+
+        for iter in range(1,1001):
             rs1 = test.get_problem(pidx).rack.status
 
             for i in range(len(input) / 2):
@@ -59,26 +60,26 @@ class ObservationGenerator(object):
                 inputs = input[(k * 2 - 2):k * 2]
                 outputs = output[(k * 2 - 2):k * 2]
                 ac = action.action()
+                idx = random.randrange(0,4)
                 sol, cycletime = ac.dijk_idx(rs1, column, floor, inputs, outputs, idx)
                 #rs2 = simul.change_rs(rs1, column, floor, sol)
                 if i == ((len(input) / 2) - 1):
                     cur.execute("""INSERT INTO """ + """%s""" % (
-                    tablename) + """ (pidx,rs,act,inp,outp,reward,rsprime,terminal) VALUES (%s,%s,%s,%s,%s,%s,%s,'O')""",
-                                (pidx, str(rs1), idx, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, sol))))
+                    tablename) + """ (iteridx,rs,act,inp,outp,reward,rsprime,terminal) VALUES (%s,%s,%s,%s,%s,%s,%s,'O')""",
+                                (iter, str(rs1), idx, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, sol))))
                     con.commit()
                     rs1 = rs
                 else:
                     cur.execute("""INSERT INTO """ + """%s""" % (
-                    tablename) + """ (pidx,rs,act,inp,outp,reward,rsprime) VALUES (%s,%s,%s,%s,%s,%s,%s)""",
-                                (pidx, str(rs1), idx, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, sol))))
+                    tablename) + """ (iteridx,rs,act,inp,outp,reward,rsprime) VALUES (%s,%s,%s,%s,%s,%s,%s)""",
+                                (iter, str(rs1), idx, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, sol))))
                     con.commit()
                     rs1 = simul.change_rs(rs1, column, floor, sol)
 
 
-    def make_data_dijk(self, tidx):
-        for i in range(500):
-            self.make_table_dijk(tidx)
-            self.insert_ob_dijk(tidx,i+1)
+    def make_data_dijk(self, tidx, pidx):
+        self.make_table_dijk(tidx)
+        self.insert_ob_dijk(tidx,pidx)
 
 
 
@@ -89,7 +90,7 @@ class ObservationGenerator(object):
         sql = "CREATE TABLE IF NOT EXISTS `%s`" \
               "(" \
               "idx int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT," \
-              "pidx int(5) NULL," \
+              "iteridx int(10) NULL," \
               "rs TEXT NULL COLLATE utf8mb4_unicode_ci," \
               "act int(2) NULL DEFAULT NULL," \
               "inp TEXT NULL COLLATE utf8mb4_unicode_ci," \
@@ -119,35 +120,35 @@ class ObservationGenerator(object):
         rw = reward.reward()
         ac = action.action()
 
-        for i in range(1,12):
+        for iter in range(1,1001):
             rs1 = test.get_problem(pidx).rack.status
             for j in range(len(input) / 2):
                 k = j + 1
                 inputs = input[(k * 2 - 2):k * 2]
                 outputs = output[(k * 2 - 2):k * 2]
+                i = random.randrange(1, 12)
                 sol, cycletime = ac.dijk(rs1, column, floor, inputs, outputs)
                 new_sol = acg.generating_idx(rs1,column,floor,sol,i,fixed_oper)
                 cycletime = rw.get_cycletime(new_sol)
+
                 #rs2 = simul.change_rs(rs1, column, floor, new_sol)
                 if j == ((len(input) / 2) - 1):
                     cur.execute("""INSERT INTO """ + """%s""" % (
-                        tablename) + """ (pidx,rs,act,inp,outp,reward,rsprime,terminal) VALUES (%s,%s,%s,%s,%s,%s,%s,'O')""",
-                                (pidx, str(rs1), i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
+                        tablename) + """ (iteridx,rs,act,inp,outp,reward,rsprime,terminal) VALUES (%s,%s,%s,%s,%s,%s,%s,'O')""",
+                                (iter, str(rs1), i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
                     con.commit()
 
                 else:
                     cur.execute("""INSERT INTO """ + """%s""" % (
-                        tablename) + """ (pidx,rs,act,inp,outp,reward,rsprime) VALUES (%s,%s,%s,%s,%s,%s,%s)""",
-                                (pidx, str(rs1), i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
+                        tablename) + """ (iteridx,rs,act,inp,outp,reward,rsprime) VALUES (%s,%s,%s,%s,%s,%s,%s)""",
+                                (iter, str(rs1), i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
                     con.commit()
                     rs1 = simul.change_rs(rs1, column, floor, new_sol)
 
-    def make_data_fixed_operation(self, tidx):
-        for j in range(500):
-
-            for i in range(4):
-                self.make_table_fixed_operation(i)
-                self.insert_ob_fixed_operation(tidx,j+1,i)
+    def make_data_fixed_operation(self, tidx, pidx):
+        for i in range(4):
+            self.make_table_fixed_operation(i)
+            self.insert_ob_fixed_operation(tidx,pidx,i)
 
     def make_table_fixed_action(self, fixed_act):
         tablename = 'fixed_action_' + str(fixed_act)
@@ -156,7 +157,7 @@ class ObservationGenerator(object):
         sql = "CREATE TABLE IF NOT EXISTS `%s`" \
               "(" \
               "idx int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT," \
-              "pidx int(5) NULL," \
+              "iteridx int(10) NULL," \
               "rs TEXT NULL COLLATE utf8mb4_unicode_ci," \
               "act int(2) NULL DEFAULT NULL," \
               "operation int(1) NULL DEFAULT NULL," \
@@ -188,12 +189,13 @@ class ObservationGenerator(object):
         ac = action.action()
 
 
-        for i in range(4):
+        for iter in range(1, 1001):
             rs1 = test.get_problem(pidx).rack.status
             for j in range(len(input) / 2):
                 k = j + 1
                 inputs = input[(k * 2 - 2):k * 2]
                 outputs = output[(k * 2 - 2):k * 2]
+                i = random.randrange(0,4)
                 sol, cycletime = ac.dijk(rs1, column, floor, inputs, outputs)
                 new_sol = acg.generating_idx(rs1, column, floor, sol, fixed_act, i)
                 cycletime = rw.get_cycletime(new_sol)
@@ -201,28 +203,25 @@ class ObservationGenerator(object):
 
                 if j == ((len(input) / 2) - 1):
                     cur.execute("""INSERT INTO """ + """%s""" % (
-                        tablename) + """ (pidx,rs,act,operation,inp,outp,reward,rsprime,terminal) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,'O')""",
-                                (pidx, str(rs1), fixed_act, i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
+                        tablename) + """ (iteridx,rs,act,operation,inp,outp,reward,rsprime,terminal) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,'O')""",
+                                (iter, str(rs1), fixed_act, i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
                     con.commit()
 
                 else:
                     cur.execute("""INSERT INTO """ + """%s""" % (
-                        tablename) + """ (pidx,rs,act,operation,inp,outp,reward,rsprime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",
-                                (pidx, str(rs1), fixed_act, i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
+                        tablename) + """ (iteridx,rs,act,operation,inp,outp,reward,rsprime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",
+                                (iter, str(rs1), fixed_act, i, str(inputs), str(outputs), cycletime, str(simul.change_rs(rs1, column, floor, new_sol))))
                     con.commit()
                     rs1 = simul.change_rs(rs1, column, floor, new_sol)
 
-    def make_data_fixed_action(self, tidx):
-
-        for j in range(500):
-
-            for i in range(1,12):
-                self.make_table_fixed_action(i)
-                self.insert_ob_fixed_action(tidx,j+1,i)
+    def make_data_fixed_action(self, tidx, pidx):
+         for i in range(1,12):
+            self.make_table_fixed_action(i)
+            self.insert_ob_fixed_action(tidx,pidx,i)
 
 
-
-a = ObservationGenerator()
-a.make_data_dijk(20)
-a.make_data_fixed_operation(20)
-a.make_data_fixed_action(20)
+if __name__ == '__main__':
+    a = ObservationGenerator()
+    #a.make_data_dijk(20,1)
+    #a.make_data_fixed_operation(20,1)
+    a.make_data_fixed_action(20,1)
