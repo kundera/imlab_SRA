93b623dab7222f57aa080340eb4cec62f4d23ece
SukhyunCho <chosh90@snu.ac.kr>
2016-05-30 13:05:02 +0900

    shortest path action

--- learner/heuristic.py
+++ learner/heuristic.py
@@ -68,7 +68,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[0] = lot[b]
                     distance = new_distance
@@ -86,7 +86,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for d in range(len(lot)):
-                new_distance = math.pow((lot[d][1] - path[0][1]), 2) + math.pow((lot[d][2] - path[0][2]), 2)
+                new_distance = self.get_time(path[0], lot[d])
                 if distance > new_distance and path[0] != lot[d]:
                     path[1] = lot[d]
                     distance = new_distance
@@ -104,7 +104,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for f in range(len(lot)):
-                new_distance = math.pow((lot[f][1] - path[1][1]), 2) + math.pow((lot[f][2] - path[1][2]), 2)
+                new_distance = self.get_time(path[1], lot[f])
                 if distance > new_distance:
                     path[2] = lot[f]
                     distance = new_distance
@@ -122,7 +122,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[2][1]), 2) + math.pow((lot[h][2] - path[2][2]), 2)
+                new_distance = self.get_time(path[2], lot[h])
                 if distance > new_distance and path[2] != lot[h]:
                     path[3] = lot[h]
                     distance = new_distance
@@ -160,7 +160,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[0] = lot[b]
                     distance = new_distance
@@ -178,7 +178,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for d in range(len(lot)):
-                new_distance = math.pow((lot[d][1] - path[0][1]), 2) + math.pow((lot[d][2] - path[0][2]), 2)
+                new_distance = self.get_time(path[0], lot[d])
                 if distance > new_distance and path[0] != lot[d]:
                     path[1] = lot[d]
                     distance = new_distance
@@ -196,7 +196,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for f in range(len(lot)):
-                new_distance = math.pow((lot[f][1] - path[1][1]), 2) + math.pow((lot[f][2] - path[1][2]), 2)
+                new_distance = self.get_time(path[1], lot[f])
                 if distance > new_distance:
                     path[2] = lot[f]
                     distance = new_distance
@@ -214,7 +214,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[2][1]), 2) + math.pow((lot[h][2] - path[2][2]), 2)
+                new_distance = self.get_time(path[2], lot[h])
                 if distance > new_distance and path[2] != lot[h]:
                     path[3] = lot[h]
                     distance = new_distance
@@ -252,7 +252,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[0] = lot[b]
                     distance = new_distance
@@ -270,7 +270,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for f in range(len(lot)):
-                new_distance = math.pow((lot[f][1] - path[0][1]), 2) + math.pow((lot[f][2] - path[0][2]), 2)
+                new_distance = self.get_time(path[0], lot[f])
                 if distance > new_distance:
                     path[1] = lot[f]
                     path[2] = lot[f]
@@ -308,7 +308,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[2][1]), 2) + math.pow((lot[h][2] - path[2][2]), 2)
+                new_distance = self.get_time(path[2], lot[h])
                 if distance > new_distance and path[2] != lot[h]:
                     path[3] = lot[h]
                     distance = new_distance
@@ -346,7 +346,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[0] = lot[b]
                     distance = new_distance
@@ -364,7 +364,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for f in range(len(lot)):
-                new_distance = math.pow((lot[f][1] - path[0][1]), 2) + math.pow((lot[f][2] - path[0][2]), 2)
+                new_distance = self.get_time(path[0], lot[f])
                 if distance > new_distance:
                     path[1] = lot[f]
                     path[2] = lot[f]
@@ -401,7 +401,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[2][1]), 2) + math.pow((lot[h][2] - path[2][2]), 2)
+                new_distance = self.get_time(path[2], lot[h])
                 if distance > new_distance and path[2] != lot[h]:
                     path[3] = lot[h]
                     distance = new_distance
@@ -439,7 +439,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[3] = lot[b]
                     distance = new_distance
@@ -457,7 +457,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for d in range(len(lot)):
-                new_distance = math.pow((lot[d][1] - path[3][1]), 2) + math.pow((lot[d][2] - path[3][2]), 2)
+                new_distance = self.get_time(path[3], lot[d])
                 if distance > new_distance and path[3] != lot[d]:
                     path[2] = lot[d]
                     distance = new_distance
@@ -475,7 +475,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for f in range(len(lot)):
-                new_distance = math.pow((lot[f][1] - path[2][1]), 2) + math.pow((lot[f][2] - path[2][2]), 2)
+                new_distance = self.get_time(path[2], lot[f])
                 if distance > new_distance:
                     path[1] = lot[f]
                     distance = new_distance
@@ -493,7 +493,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[1][1]), 2) + math.pow((lot[h][2] - path[1][2]), 2)
+                new_distance = self.get_time(path[1], lot[h])
                 if distance > new_distance and path[1] != lot[h]:
                     path[0] = lot[h]
                     distance = new_distance
@@ -531,7 +531,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[3] = lot[b]
                     distance = new_distance
@@ -549,7 +549,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for d in range(len(lot)):
-                new_distance = math.pow((lot[d][1] - path[3][1]), 2) + math.pow((lot[d][2] - path[3][2]), 2)
+                new_distance = self.get_time(path[3], lot[d])
                 if distance > new_distance and path[3] != lot[d]:
                     path[2] = lot[d]
                     distance = new_distance
@@ -567,7 +567,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for f in range(len(lot)):
-                new_distance = math.pow((lot[f][1] - path[2][1]), 2) + math.pow((lot[f][2] - path[2][2]), 2)
+                new_distance = self.get_time(path[2], lot[f])
                 if distance > new_distance:
                     path[1] = lot[f]
                     distance = new_distance
@@ -585,7 +585,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[1][1]), 2) + math.pow((lot[h][2] - path[1][2]), 2)
+                new_distance = self.get_time(path[1], lot[h])
                 if distance > new_distance and path[1] != lot[h]:
                     path[0] = lot[h]
                     distance = new_distance
@@ -623,7 +623,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[3] = lot[b]
                     distance = new_distance
@@ -641,7 +641,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for d in range(len(lot)):
-                new_distance = math.pow((lot[d][1] - path[3][1]), 2) + math.pow((lot[d][2] - path[3][2]), 2)
+                new_distance = self.get_time(path[3], lot[d])
                 if distance > new_distance and path[3] != lot[d]:
                     path[2] = lot[d]
                     distance = new_distance
@@ -660,7 +660,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[1][1]), 2) + math.pow((lot[h][2] - path[1][2]), 2)
+                new_distance = self.get_time(path[1], lot[h])
                 if distance > new_distance and path[1] != lot[h]:
                     path[0] = lot[h]
                     distance = new_distance
@@ -698,7 +698,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for b in range(len(lot)):
-                new_distance = math.pow((lot[b][1] - 0), 2) + math.pow((lot[b][2] - 0), 2)
+                new_distance = self.get_time([0, 0, 0], lot[b])
                 if distance > new_distance:
                     path[3] = lot[b]
                     distance = new_distance
@@ -716,7 +716,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for d in range(len(lot)):
-                new_distance = math.pow((lot[d][1] - path[3][1]), 2) + math.pow((lot[d][2] - path[3][2]), 2)
+                new_distance = self.get_time(path[3], lot[d])
                 if distance > new_distance and path[3] != lot[d]:
                     path[2] = lot[d]
                     distance = new_distance
@@ -735,7 +735,7 @@ class heuristics(object):
             distance = 10000
             lot.sort()
             for h in range(len(lot)):
-                new_distance = math.pow((lot[h][1] - path[1][1]), 2) + math.pow((lot[h][2] - path[1][2]), 2)
+                new_distance = self.get_time(path[1], lot[h])
                 if distance > new_distance and path[1] != lot[h]:
                     path[0] = lot[h]
                     distance = new_distance
@@ -743,6 +743,74 @@ class heuristics(object):
         sol = solution.solution(path, item, io)
         cycletime = self.get_cycletime(sol)
         # print path, item, io, cycletime
+        return sol, cycletime
+
+    def shortest_path(self, rs, column, floor, input, output):
+        rack = rs
+        size_h = column
+        size_v = floor
+
+        s1 = input[0]
+        s2 = input[1]
+        r1 = output[0]
+        r2 = output[1]
+        item = [s1, r1, s2, r2]
+        io = ['s1', 'r1', 's2', 'r2']
+
+        path = ['first', 'second', 'third', 'fourth']
+
+        # s1 - r1
+        lot1 = []
+        for a, item1 in enumerate(rack):
+            if item1 == -1:
+                loca1 = self.loca_calculate(a, size_h, size_v)
+                lot1.append(loca1)
+
+        if lot1 == []:
+            print 'no empty space'
+        else:
+            lot2 = []
+            for b, item3 in enumerate(rack):
+                if item3 == r1:
+                    loca3 = self.loca_calculate(b, size_h, size_v)
+                    lot2.append(loca3)
+
+            if lot2 == []:
+                print 'no target item'
+            else:
+                distance = 10000
+                lot1.sort()
+                lot2.sort()
+                for c in range(len(lot1)):
+                    for d in range(len(lot2)):
+                        new_distance = self.get_time([0, 0, 0], lot1[c]) + self.get_time(lot1[c], lot2[d])
+                        if distance > new_distance:
+                            path[0] = lot1[c]
+                            path[1] = lot2[d]
+                            distance = new_distance
+                            # s2
+                            path[2] = lot2[d]
+        # r2
+        lot3 = []
+        for e, item4 in enumerate(rack):
+            if item4 == r2:
+                loca4 = self.loca_calculate(e, size_h, size_v)
+                lot3.append(loca4)
+
+        if lot3 == []:
+            print 'no target item'
+        else:
+            distance = 10000
+            lot3.sort()
+            for f in range(len(lot3)):
+                new_distance = self.get_time([0, 0, 0], lot3[f]) + self.get_time(lot3[f], path[2])
+                if distance > new_distance and path[2] != lot3[f]:
+                    path[3] = lot3[f]
+                    distance = new_distance
+
+        sol = solution.solution(path, item, io)
+        cycletime = self.get_cycletime(sol)
+
         return sol, cycletime
 
     def nearest_idx(self, rs, column, floor, input, output, idx):
@@ -847,6 +915,39 @@ class heuristics(object):
             a = self.reverse_nearest_neighbor_s1r1s2r2(rack1, column, floor, input, output)
             return a
 
+    def shortest_path_density_idx(self, rs, column, floor, input, output, idx):
+        rack1 = copy.deepcopy(rs)
+        # srsr
+        # 0 - high low high low
+        # 1 - low high high low
+        # 2 - high low low high
+        # 3 - low high low high
+
+        if idx == 0 or idx == 2:
+            if rs.count(input[0]) >= rs.count(input[1]):
+                input = [input[0], input[1]]
+            else:
+                input = [input[1], input[0]]
+        elif idx == 1 or idx == 3:
+            if rs.count(input[0]) >= rs.count(input[1]):
+                input = [input[1], input[0]]
+            else:
+                input = [input[0], input[1]]
+
+        if idx == 0 or idx == 1:
+            if rs.count(output[0]) >= rs.count(output[1]):
+                output = [output[0], output[1]]
+            else:
+                output = [output[1], output[0]]
+        elif idx == 2 or idx == 3:
+            if rs.count(output[0]) >= rs.count(output[1]):
+                output = [output[1], output[0]]
+            else:
+                output = [output[0], output[1]]
+
+        a = self.shortest_path(rack1, column, floor, input, output)
+        return a
+
 if __name__ == '__main__':
 
     test = problemreader.ProblemReader(23)
@@ -855,11 +956,10 @@ if __name__ == '__main__':
     floor = test.get_problem(1).rack.floor
 
     ts1 = heuristics()
-    # a = ts1.nearest_idx(rs, column, floor, [123, 456], [28, 29], 1)
+    # a = ts1.shortest_path(rs, column, floor, [123, 456], [28, 29])
     # print a[0].loc, a[0].type, a[0].oper, a[1]
-    for b in range(8):
-        a = ts1.nearest_density_idx(rs, column, floor, [22, 21], [28, 29], b)
-        print a[0].loc, a[0].type, a[0].oper, a[1]
-        a = ts1.reverse_nearest_density_idx(rs, column, floor, [22, 21], [28, 29], b)
+    print rs
+    for b in range(4):
+        a = ts1.shortest_path_density_idx(rs, column, floor, [22, 28], [27, 15], b)
         print a[0].loc, a[0].type, a[0].oper, a[1]
         print
